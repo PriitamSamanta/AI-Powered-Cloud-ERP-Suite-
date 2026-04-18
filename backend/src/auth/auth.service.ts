d34f6567e006357777e0671d93147649259097ea
PriitamSamanta <pritamsamanta506@gmail.com>
2026-04-18 19:45:45 +0530

    initial auth module completed

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) throw new Error('User not found');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) throw new Error('Invalid password');
+
+    const token = this.jwtService.sign({
+      userId: user.id,
+      email: user.email,
+    });
+
+    return {
+      message: 'Login successful',
+      access_token: token,
+    };
+  }
+}
