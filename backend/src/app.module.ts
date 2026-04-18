d9581a30840745cd09966f62989bc568d7f579f3
PriitamSamanta <pritamsamanta506@gmail.com>
2026-04-18 19:47:02 +0530

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [PrismaModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
